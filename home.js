cc8784d64b53cfae1e5c62a82f7ec72aeab95451
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-06-21 10:04:26 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
-// Q1 print the result 
-// npm i request jsdom 
+ // npm i request jsdom 
 const request = require('request');
 const fs = require("fs");
 const path = require("path");
@@ -15,7 +14,7 @@ helperObj.dirCreater(iplPath);
 request(url, cb);
 function cb(error, response, body) {
     if (error) {
-        console.log('error:', error.message); // Print the error message
+        console.log('error:', error.message);
     } else if (response && response.statusCode == 404) {
         console.log("Page not found");
     } else {
@@ -26,10 +25,7 @@ function cb(error, response, body) {
 }
 function extractData(body) {
     const JSDOM = jsdom.JSDOM;
-    // pass to newJSDOM 
     let dom = new JSDOM(body);
-    // 2. // no meaning 
-    // document represent the whole html page 
     let document = dom.window.document;
 
     let element = document.querySelector(".ds-block.ds-text-center.ds-uppercase.ds-text-ui-typo-primary.ds-underline-offset-4")

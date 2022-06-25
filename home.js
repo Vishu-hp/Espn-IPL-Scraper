9e69e332456015791df562c8b348440123ea75bd
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-06-25 17:07:33 +0530

    Update home.js

--- home.js
+++ home.js
@@ -10,7 +10,7 @@ let url = "https://www.espncricinfo.com/series/ipl-2021-1249214";
 let iplPath = path.join(__dirname, "ipl");
 
 helperObj.dirCreater(iplPath);
-//  first request
+
 request(url, cb);
 function cb(error, response, body) {
     if (error) {
@@ -19,7 +19,6 @@ function cb(error, response, body) {
         console.log("Page not found");
     } else {
         console.log("content recieved");
-        // console.log(body);
         extractData(body);
     }
 }
@@ -30,7 +29,7 @@ function extractData(body) {
 
     let element = document.querySelector(".ds-block.ds-text-center.ds-uppercase.ds-text-ui-typo-primary.ds-underline-offset-4")
     let link = element.getAttribute("href");
-    // console.log("link", link);
+    
     let AllMatchPageKaLink = "https://www.espncricinfo.com" + link;
     console.log(AllMatchPageKaLink);
     // allmatch page
